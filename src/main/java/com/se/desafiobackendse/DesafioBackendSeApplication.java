c925b86a79ecacf5ac1af8ffe22d13be4d5f6a13
bruno silva <brunoliracz@gmail.com>
2023-06-24 19:38:20 -0300

    init project

---
+++ src/main/java/com/se/desafiobackendse/DesafioBackendSeApplication.java
@@ -0,0 +1,13 @@
+package com.se.desafiobackendse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendSeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendSeApplication.class, args);
+	}
+
+}
