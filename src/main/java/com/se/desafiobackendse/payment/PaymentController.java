d134124837ca82684444f7be19b5032ac14fb09b
bruno silva <brunoliracz@gmail.com>
2023-06-28 18:29:54 -0300

    Add redirect endpoint

--- src/main/java/com/se/desafiobackendse/payment/PaymentController.java
+++ src/main/java/com/se/desafiobackendse/payment/PaymentController.java
@@ -2,10 +2,15 @@ package com.se.desafiobackendse.payment;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 
 @RestController
 @RequestMapping("/payment")
@@ -17,6 +22,14 @@ public class PaymentController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PaymentResponse> getPaymentLink(PaymentRequest paymentRequest) {
         return ResponseEntity.ok(paymentService.getPaymentLink(paymentRequest));
+    }
+
+    @GetMapping("/redirect")
+    public ResponseEntity<?> redirect(PaymentRequest paymentRequest) throws URISyntaxException {
+
+        URI redirect = new URI(paymentService.getPaymentLink(paymentRequest).paymentLink());
+        return ResponseEntity.status(HttpStatus.SEE_OTHER)
+                .location(redirect).build();
     }
 
 }
